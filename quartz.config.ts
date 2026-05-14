57ba9e74b753c4269bdb26a95c396f902e4d92a1
Admin <annahoch@Mac.fritz.box>
2026-05-14 13:00:00 +0200

    Initial garden setup

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,16 +16,16 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ravengreen.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "League Spartan",
+        body: "Lora",
+        code: "JetBrains Mono",
       },
       colors: {
         lightMode: {
